7970f73c06bbcc271cc91febac90909379e57aaf
Reyzdor <m3f3dr0nbristleback@gmail.com>
2025-09-10 21:45:40 +0300

    fix

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Header from "./components/Header"
+import { ways } from "./data"
+import WayToTeach from "./components/WayToTeach"
+
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <header>
+        <h3>Result</h3>
+
+      </header>
+
+      <main>
+      <section>
+        <ul>
+        <WayToTeach title={ways[0].title} description={ways[0].description}/>
+        <WayToTeach {... ways[1]} />
+        <WayToTeach {... ways[2]} />
+        <WayToTeach {... ways[3]} />
+        </ul>
+      </section>
+        
+      </main>
+    </div>
+  )
+}
